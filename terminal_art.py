41212b1c3ba4026154b4df747148208d75f130c1
Hew <hew.phipps@keble.ox.ac.uk>
2025-07-07 18:50:00 +0100

    Added functionality for different iamge inputs and argument based output adjustment

--- terminal_art.py
+++ terminal_art.py
@@ -1,13 +1,34 @@
 from PIL import Image
 import numpy as np
+import argparse
 
-IMAGE = "/home/hphipps_/terminal_art/burial_crop.jpg"
+parser = argparse.ArgumentParser(description="Simple jpg -> terminal art processor")
+parser.add_argument("-f", type=str, required=True, help="Provide input jpg filepath")
+parser.add_argument("--cutoff", action="store_true", help="lowers brightness of left 1/3rd")
+parser.add_argument("--inverse", action="store_true", help="inverts brightness")
+parser.add_argument("--padding", type=float, required=False, default=0.0, help="shifts image to right by a factor of arg times its width")
+parser.add_argument("--resize", type=float, required=False, default=1.0, help="multiplicative image dimensions resizer")
+parser.add_argument("--colour", type=int, required=False, default=36, help="set text colour using ascii escape code integer values e.g. 36 = cyan")
+
+args = parser.parse_args()
+
+#DIR = "/home/hphipps_/terminal_art"
+IMAGE = args.f
+print("processing", IMAGE)
 ASCIIs = ' `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 MAX_BRIGHTNESS = 255
+RESIZE=args.resize
 
 image = Image.open(IMAGE)
-image = image.resize((int(0.1*image.width), int(0.1*image.height)))
+image = image.resize((int(RESIZE*image.width), int(RESIZE*image.height)))
 pixels = image.load()
+
+not_rgb = False
+try:
+    r,g,b = pixels[0,0]
+except:
+    print("not rgb")
+    not_rgb = True
 
 def average(rgb):
     r, g, b = rgb
@@ -26,20 +47,29 @@ def contrast(rgb):
     # Normalize to 0–1
     norm = luminance / 255.0
     # Apply gamma correction to boost contrast
-    gamma =  7 # < 1 brightens highlights, darkens shadows
+    if args.inverse:
+        gamma = 0.2    
+    else: gamma = 7 # < 1 brightens highlights, darkens shadows
     boosted = norm ** gamma
     # Rescale to 0–255
     return int(boosted * 255)
 
 method = contrast
 
-matrix = np.zeros((image.height, image.width), dtype=np.float64)
-for i in range(image.height):
-    for j in range(image.width):
-        if i == 0 or i == image.height:
-            matrix[i,j] = 255
-        else:
-            matrix[i,j] = method(pixels[j,i])
+if not_rgb == False:
+    matrix = np.zeros((image.height, image.width), dtype=np.float64)
+    for i in range(image.height):
+        for j in range(image.width):
+            if i == 0 or i == image.height:
+                matrix[i,j] = 255
+            else:
+                matrix[i,j] = method(pixels[j,i])
+else: matrix = np.array(image)
+
+matrix = np.pad(matrix, pad_width=((0,0), (int(args.padding*image.width), 0)), mode="constant", constant_values=0.0) 
+
+if args.inverse:
+    matrix = 255 - matrix
 
 def map_val_to_ascii(val):
     space = MAX_BRIGHTNESS/len(ASCIIs)
@@ -48,18 +78,22 @@ def map_val_to_ascii(val):
     #    char = " "
     return char
 
-ascii_matrix = np.zeros((image.height, image.width), dtype=str)
-for i in range(image.height):
-    for j in range(image.width):
-        if i == 0 or i == image.height-1:
+ascii_matrix = np.zeros((np.shape(matrix)[0], np.shape(matrix)[1]), dtype=str)
+for i in range(np.shape(matrix)[0]):
+    for j in range(np.shape(matrix)[1]):
+        if i == 0 or i == np.shape(matrix)[0]-1:
             char = map_val_to_ascii(5)
         else:
-            if j < 0.3*image.width:
-                char = map_val_to_ascii(0.1*matrix[i,j])
+            if args.cutoff and j < 0.3*np.shape(matrix)[1]:
+                if args.inverse:
+                     val = 0
+                else: val = 0.1*matrix[i,j]
+                char = map_val_to_ascii(val)
             else: 
-                char = f"\033[36m{map_val_to_ascii(matrix[i,j])}\033[0m"
+                char = f"\033[{args.colour}m{map_val_to_ascii(matrix[i,j])}\033[0m"
         print(char, end="")
         print(char, end="")
         print(char, end="")
         ascii_matrix[i,j] = char
     print("")
+
